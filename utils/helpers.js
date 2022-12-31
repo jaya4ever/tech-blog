38df841e032c8370c3b76d807486abb149c73303
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2022-12-31 15:07:19 -0600

    worked on helper.js utils

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,4 +2,18 @@ module.exports = {
     format_time: (date) => {
       return date.toLocaleTimeString();
     },
+    formate_amount: (amount) => {
+        return parseInt(amount).toLocaleString();
+    },
+    get_emoji: () => {
+        const randomNum = Math.random();
+        if(randomNum > 0.7){
+            return `<span></span>`;
+        } else if (randomNum > 0.4){
+            return `<span></span>`;
+        }else {
+            return `<span></span>`;
+        }
+
+    },
 };
