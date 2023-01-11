c90442bdf3b1023847d270d8ecafa334d5f52890
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2023-01-10 19:29:02 -0600

    fixed helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,19 +1,6 @@
 module.exports = {
-    format_time: (date) => {
-      return date.toLocaleTimeString();
-    },
-    formate_amount: (amount) => {
-        return parseInt(amount).toLocaleString();
-    },
-    get_emoji: () => {
-        const randomNum = Math.random();
-        if(randomNum > 0.7){
-            return `<span></span>`;
-        } else if (randomNum > 0.4){
-            return `<span></span>`;
-        }else {
-            return `<span></span>`;
-        }
-
-    },
-};
+    format_date: date => {
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
+      },
+  
+  }
